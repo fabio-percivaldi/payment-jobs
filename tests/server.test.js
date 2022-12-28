4ab906fd4277eecf1c39a444ff51cae89582bd91
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2022-12-28 22:22:23 +0100

    added a test for 401 error case

--- tests/server.test.js
+++ tests/server.test.js
@@ -17,12 +17,20 @@ tap.test('GET contracts', async test => {
     assert.end()
   })
 
-  test.test('401 - return error for wrong contractor id', async assert => {
+  test.test('404 - return error for wrong contractor id', async assert => {
     const response = await request(app)
       .get('/contracts/1')
       .set('profile_id', 1)
 
     assert.equal(response.statusCode, 404)
+    assert.end()
+  })
+
+  test.test('401 - return error if not profile id is sent', async assert => {
+    const response = await request(app)
+      .get('/contracts/1')
+
+    assert.equal(response.statusCode, 401)
     assert.end()
   })
   test.end()
