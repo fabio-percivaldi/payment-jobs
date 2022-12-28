febc1da39b050b4e0e7ac685fe9d45b5e2e9dbe7
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2022-12-28 22:38:47 +0100

    refactoring: dividing routes in submodules

--- src/app.js
+++ src/app.js
@@ -3,34 +3,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { sequelize } = require('./model')
-const { getProfile } = require('./middleware/getProfile')
 const app = express()
 app.use(bodyParser.json())
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
-const { Op } = require('sequelize')
+const contractsRoutes = require('./routes/contracts')
 
-/**
- * @returns contract by id
- */
-app.get('/contracts/:id', getProfile, async(req, res) => {
-  const { Contract } = req.app.get('models')
-  const { id } = req.params
-  const { profile } = req
-  const contract = await Contract.findOne({ where: { id, ContractorId: profile.id } })
-  if (!contract) { return res.status(404).end() }
-  res.json(contract)
-})
+app.use(contractsRoutes)
 
-app.get('/contracts', getProfile, async(req, res) => {
-  const { Contract } = req.app.get('models')
-  const { profile } = req
-  const contract = await Contract.findAll({ where: { ContractorId: profile.id,
-    [Op.or]: [
-      { status: 'new' },
-      { status: 'in_progress' },
-    ] } })
-  if (!contract) { return res.status(404).end() }
-  res.json(contract)
-})
 module.exports = app
