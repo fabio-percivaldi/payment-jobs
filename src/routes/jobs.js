0f6a812a959aa9dcaae4ac01f7ca0ff1016e4a57
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2022-12-29 00:14:44 +0100

    added happy path for pay

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -2,17 +2,18 @@
 
 const express = require('express')
 const router = express.Router()
+const logger = require('pino')()
+const { sequelize } = require('../model')
 const { Op } = require('sequelize')
 const { getProfile } = require('../middleware/getProfile')
 const {
-  PROFILE_TYPES, CONTRACT_STATUS, Job,
+  PROFILE_TYPES, CONTRACT_STATUS, Job, Contract, Profile,
 } = require('../model')
 
 /**
- * @returns contract by id
+ * @returns list unpaid jobs
  */
 router.get('/jobs/unpaid', getProfile, async(req, res) => {
-  const { Contract } = req.app.get('models')
   const { profile } = req
   const { id: profileId, type } = profile
 
@@ -45,6 +46,82 @@ router.get('/jobs/unpaid', getProfile, async(req, res) => {
   if (!contracts) { return res.status(404).end() }
   const jobs = contracts.map(contract => contract.Jobs)
   res.json(jobs.flat())
+})
+
+const payJob = async(job, clientProfile, contractorProfile) => {
+  const paymentTransaction = await sequelize.transaction()
+
+  try {
+    job.paid = true
+    job.paymentDate = new Date()
+    await job.save()
+  } catch (error) {
+    logger.error({ error: error.message }, 'Error while updating payment')
+    await paymentTransaction.rollback()
+  }
+  try {
+    clientProfile.balance -= job.price
+    await clientProfile.save()
+  } catch (error) {
+    logger.error({ error: error.message }, 'Error while updating client balance')
+    await paymentTransaction.rollback()
+  }
+  try {
+    contractorProfile.balance += job.price
+    await contractorProfile.save()
+  } catch (error) {
+    logger.error({ error: error.message }, 'Error while updating contractor balance')
+    await paymentTransaction.rollback()
+  }
+  await paymentTransaction.commit()
+}
+
+/**
+ * @returns pay a job
+ */
+router.post('/jobs/:job_id/pay', getProfile, async(req, res) => {
+  const { profile } = req
+  const { job_id: jobId } = req.params
+
+  const { balance, type } = profile
+
+  if (type !== PROFILE_TYPES.CLIENT) {
+    logger.error('Only clients can pay jobs')
+    res.status(401).json({
+      error: 'UNAUTHORIZED_PAY',
+      errorCode: '4001',
+      message: 'Only clients can pay jobs',
+    })
+  }
+
+  const job = await Job.findOne({ where: { id: jobId } })
+
+  if (!job) { return res.status(404).end() }
+  const {
+    price,
+    ContractId,
+  } = job
+
+  if (balance >= price) {
+    const contract = await Contract.findOne({ where: { id: ContractId },
+      include: {
+        model: Profile,
+        as: 'Contractor',
+      } })
+
+    const { Contractor: contractor } = contract
+    try {
+      await payJob(job, profile, contractor)
+    } catch (error) {
+      logger.error({ error: error.message, job: jobId }, 'Error while paying job')
+      res.status(500).json({
+        error: 'PAYMENT_ERROR',
+        errorCode: '5001',
+        message: 'Error while paying a job',
+      })
+    }
+  }
+  res.status(201).end()
 })
 
 module.exports = router
--- tests/jobs.test.js
+++ tests/jobs.test.js
@@ -9,7 +9,7 @@ const { Job, Profile } = require('../src/model')
 app.use(bodyParser.json())
 
 tap.test('GET /jobs/unpaid by id', async test => {
-  test.test('200 - return the list of upaid jobs given a client id', async assert => {
+  test.test('200 - return the list of unpaid jobs given a client id', async assert => {
     // TODO change the check on createdAt and updatedAt
     const expectedBody = [{
       id: 2,
@@ -18,8 +18,8 @@ tap.test('GET /jobs/unpaid by id', async test => {
       paid: null,
       paymentDate: null,
       ContractId: 2,
-      createdAt: '2022-12-28T16:22:19.621Z',
-      updatedAt: '2022-12-28T16:22:19.621Z',
+      createdAt: '2022-12-28T23:05:03.205Z',
+      updatedAt: '2022-12-28T23:05:03.205Z',
     }]
 
     const response = await request(app)
@@ -31,7 +31,7 @@ tap.test('GET /jobs/unpaid by id', async test => {
     assert.end()
   })
 
-  test.test('200 - return the list of upaid jobs given a contractor id', async assert => {
+  test.test('200 - return the list of unpaid jobs given a contractor id', async assert => {
     // TODO change the check on createdAt and updatedAt
     const expectedBody = [{
       id: 2,
@@ -40,8 +40,8 @@ tap.test('GET /jobs/unpaid by id', async test => {
       paid: null,
       paymentDate: null,
       ContractId: 2,
-      createdAt: '2022-12-28T16:22:19.621Z',
-      updatedAt: '2022-12-28T16:22:19.621Z',
+      createdAt: '2022-12-28T23:05:03.205Z',
+      updatedAt: '2022-12-28T23:05:03.205Z',
     },
     {
       id: 3,
@@ -49,8 +49,8 @@ tap.test('GET /jobs/unpaid by id', async test => {
       price: 202,
       paid: null,
       paymentDate: null,
-      createdAt: '2022-12-28T16:22:19.621Z',
-      updatedAt: '2022-12-28T16:22:19.621Z',
+      createdAt: '2022-12-28T23:05:03.205Z',
+      updatedAt: '2022-12-28T23:05:03.205Z',
       ContractId: 3,
     }]
 
@@ -70,27 +70,27 @@ const createUnpaidJob = async() => {
   const job = await Job.create({
     description: 'work',
     price: 200,
-    ContractId: 2,
+    ContractId: 4,
   })
   return job.id
 }
 
 const depositFunds = async() => {
-  await Profile.update({ balance: 1000 }, { id: 1 })
+  await Profile.update({ balance: 1000 }, { where: { id: 2 } })
 }
 
 tap.test('POST /jobs/:job_id/pay a job', async test => {
   const jobId = await createUnpaidJob()
   await depositFunds()
 
-  test.test('200 - balance is > than pay amount', async assert => {
+  test.test('201 - balance is > than pay amount', async assert => {
     const response = await request(app)
       .post(`/jobs/${jobId}/pay`)
-      .set('profile_id', 1)
+      .set('profile_id', 2)
 
-    assert.equal(response.statusCode, 200)
+    assert.equal(response.statusCode, 201)
 
-    const profile = Profile.findOne({ where: { id: 1 } })
+    const profile = await Profile.findOne({ where: { id: 2 } })
     assert.equal(profile.balance, 800)
     assert.end()
   })
